6dcae71ad3d496f6bc888b3eea88e7d8db4c1519
Orasz <daniele.ventura5@studio.unibo.it>
2019-05-13 13:58:24 +0200

    tine pages integration

--- HappyPass/HappyPass/Page4.xaml.cs
+++ HappyPass/HappyPass/Page4.xaml.cs
@@ -15,7 +15,31 @@ namespace HappyPass
 		public Page4 ()
 		{
 			InitializeComponent ();
-          //Emailframe.Content = new Entry { Text = "Email :         " , TextColor=Color.Black};   
-		}
-	}
+            for(int i = 6;i < 100; i++)
+            {
+                AgePicker.Items.Add(i.ToString());
+            }
+            for(int i = 0; i<4; i++)
+            {
+                KidsPicker.Items.Add(i.ToString());
+            }
+            KidsPicker.Items.Add("4+");
+            SexPicker.Items.Add("Male");
+            SexPicker.Items.Add("Female");
+            CountryPicker.Items.Add("Norway");
+            CountryPicker.Items.Add("Sweden");
+            CountryPicker.Items.Add("Denmark");
+            CountryPicker.Items.Add("Finland");
+            CountryPicker.Items.Add("Germany");
+            CountryPicker.Items.Add("Italy");
+            CountryPicker.Items.Add("Portugal");
+            CountryPicker.Items.Add("Spain");
+            CountryPicker.Items.Add("Holland");
+            CountryPicker.Items.Add("France");
+
+
+
+
+        }
+    }
 }
